07834fb4f72612f2066aca8e4cfe01a759d2809b
hansfpc <hansfpc@outlook.com>
2021-07-04 21:35:07 -0400

    feat: initial changes for vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
+const exportPathMap = async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+) {
+  return {
+    '/': { page: '/' },
+  }
+}
+
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['scontent-scl2-1.xx.fbcdn.net']
+  },
+  exportPathMap,
 }
